1bae5b1e2d57e3b3fecbb8fd4e4ca768550c4878
Dmitrii Grechko <prime.optimys@mail.ru>
2024-12-05 17:58:59 +0500

    error correction processing.py

--- src/processing.py
+++ src/processing.py
@@ -2,7 +2,7 @@ from typing import Dict, List
 
 
 def filter_by_state(
-    input_date_for_filter: List[Dict[str, str | int]], sorted_key: str = "EXECUTED"
+    input_data_for_filter: List[Dict[str, str | int]], sorted_key: str = "EXECUTED"
 ) -> List[Dict[str, str | int]]:
     """
     Функция принимает список словарей и опционально значение для ключа state (по умолчанию 'EXECUTED').
@@ -10,18 +10,18 @@ def filter_by_state(
     у которых ключ state соответствует указанному значению.
     """
     output_data = []
-    for i in range(len(input_date_for_filter)):
-        if input_date_for_filter[i]["state"] == sorted_key:
-            output_data.append(input_date_for_filter[i])
+    for i in range(len(input_data_for_filter)):
+        if input_data_for_filter[i]["state"] == sorted_key:
+            output_data.append(input_data_for_filter[i])
     return output_data
 
 
 def sort_by_date(
-    input_date_for_sorted: List[Dict[str, str | int]], sorted_param: bool = True
+    input_data_for_sorted: List[Dict[str, str | int]], sorted_param: bool = True
 ) -> List[Dict[str, str | int]]:
     """
     Функция принимает список словарей и необязательный параметр, задающий порядок сортировки (по умолчанию — убывание).
     Функция должна возвращать новый список, отсортированный по дате (date).
     """
-    sorted_data = sorted(input_date_for_sorted, key=lambda x: x["date"], reverse=sorted_param)
+    sorted_data = sorted(input_data_for_sorted, key=lambda x: x["date"], reverse=sorted_param)
     return sorted_data
